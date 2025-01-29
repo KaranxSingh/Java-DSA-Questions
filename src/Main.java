20d98c4ead54c5d6af4202fc139bbad17509d5d9
KaranxSingh <96904099+KaranxSingh@users.noreply.github.com>
2025-01-29 19:17:31 +0530

    linear search

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,82 @@
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//public class Main
-//{
-//    public static void main(String[] args)
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+//        int[] arr = {374,38732,938};
+        int[] arr = {18, 12, 9, 14,77, 50};
+        int target = 9;
+//
+        int ans = LinearSearch(arr, target);
+        System.out.println(ans);
+    }
+
+//    //program find target and return its index
+    static int LinearSearch(int[] arr, int target)
+    {
+
+        if(arr.length == 0)
+        {
+            return -1;
+        }
+
+        for (int index = 0; index < arr.length ; index++)
+        {
+            int index1 = arr[index];
+
+            if (target == index1)
+            {
+                return index;
+            }
+        }
+        return -1;
+
+    //----------------------------------------------------------------------------------------------
+
+    //program find target and return its value
+//    static int LinearSearch(int[] arr, int target)
 //    {
 //
-////        int[] arr = {374,38732,938};
-////        int[] arr = {18, 12, 9, 14,77, 50};
-////        int target = 9;
+//        if(arr.length == 0)
+//        {
+//            return -1;
+//        }
 //
-////        boolean ans = LinearSearch(arr, target);
-////        System.out.println(ans);
-//    }
+//        for (int index = 0; index < arr.length ; index++)
+//        {
+//            int index1 = arr[index];
 //
-////    //program find target and return its index
-////    static int LinearSearch(int[] arr, int target)
-////    {
-////
-////        if(arr.length == 0)
-////        {
-////            return -1;
-////        }
-////
-////        for (int index = 0; index < arr.length ; index++)
-////        {
-////            int index1 = arr[index];
-////
-////            if (target == index1)
-////            {
-////                return index;
-////            }
-////        }
-////        return -1;
+//            if (target == index1)
+//            {
+//                return arr[index];
+//            }
+//        }
+//        return -1;
+
+
+    //----------------------------------------------------------------------------------------------
+
+
+    //program find target and return answer in boolean
+//    static boolean LinearSearch(int[] arr, int target)
+//    {
 //
-//    //----------------------------------------------------------------------------------------------
+//        if(arr.length == 0)
+//        {
+//            return false;
+//        }
 //
-//    //program find target and return its value
-////    static int LinearSearch(int[] arr, int target)
-////    {
-////
-////        if(arr.length == 0)
-////        {
-////            return -1;
-////        }
-////
-////        for (int index = 0; index < arr.length ; index++)
-////        {
-////            int index1 = arr[index];
-////
-////            if (target == index1)
-////            {
-////                return arr[index];
-////            }
-////        }
-////        return -1;
+//        for (int index = 0; index < arr.length ; index++)
+//        {
+//            int index1 = arr[index];
 //
-//
-//    //----------------------------------------------------------------------------------------------
-//
-//
-//    //program find target and return answer in boolean
-////    static boolean LinearSearch(int[] arr, int target)
-////    {
-////
-////        if(arr.length == 0)
-////        {
-////            return false;
-////        }
-////
-////        for (int index = 0; index < arr.length ; index++)
-////        {
-////            int index1 = arr[index];
-////
-////            if (target == index1)
-////            {
-////                return true;
-////            }
-////        }
-////        return false;
-//    }
-//}
+//            if (target == index1)
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+    }
+}
--- src/RangeSearch.java
+++ src/RangeSearch.java
@@ -1,30 +1,30 @@
-//public class RangeSearch
-// {
-//    public static void main(String[] args)
-//    {
-//        int[] arr = {12,2,34,6,45,84};
-//        int target = 6;
-//         int ans = Range(arr, target, 0,3);
-//        System.out.println(ans);
-//    }
-//
-//    static int Range(int[] arr, int target, int start, int end)
-//    {
-//        if(arr.length == 0)
-//        {
-//            return -1;
-//        }
-//
-//        for(int i = start ; i <= end ; i++)
-//        {
-//            if (target == arr[i])
-//            {
-//                return arr[i];
-//            }
-//        }
-//
-//        return -1;
-// }
-//
-//
-//}
+public class RangeSearch
+ {
+    public static void main(String[] args)
+    {
+        int[] arr = {12,2,34,6,45,84};
+        int target = 6;
+         int ans = Range(arr, target, 0,3);
+        System.out.println(ans);
+    }
+
+    static int Range(int[] arr, int target, int start, int end)
+    {
+        if(arr.length == 0)
+        {
+            return -1;
+        }
+
+        for(int i = start ; i <= end ; i++)
+        {
+            if (target == arr[i])
+            {
+                return arr[i];
+            }
+        }
+
+        return -1;
+ }
+
+
+}
